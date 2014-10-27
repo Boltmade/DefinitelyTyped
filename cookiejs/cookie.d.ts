0f75186a2b938f45256e3416e9dea05c9c86a027
Justin Filip <jfilip@gmail.com>
2014-10-27 15:34:46 -0400

    Add type description for cookie.js

---
+++ cookiejs/cookie.d.ts
@@ -0,0 +1,18 @@
+// Type definitions for cookie.js
+// Project: https://github.com/js-coder/cookie.js
+// Definitions by: Boltmade <https://github.com/Boltmade>
+
+interface CookieStatic {
+  set(key : string, value : string, options? : any) : void;
+  remove(keys : string[]) : void;
+  empty() : void;
+  get(key : string) : string;
+  all() : any;
+  enabled() : boolean;
+}
+
+declare var cookie: CookieStatic;
+
+declare module "cookie" {
+  export = cookie;
+}
